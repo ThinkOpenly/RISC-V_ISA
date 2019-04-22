340f7638891559fb076b74a254e1ebc6a3cef110
Paul A. Clarke <pc@us.ibm.com>
2019-04-22 11:38:56 -0500

    Add ISA release field for each mnemonic
    A new field in the ISA.json file is added for each mnemonic,
    which describes the ISA release in which the mnemonic first
    appeared.  (Values based on canonical ISA documentation.)
    
                    "release": "P1"
                    "release": "PPC"
                    "release": "PPCAS"
                    "release": "v2.01"
                    "release": "v2.02"
                    "release": "v2.03"
                    "release": "v2.05"
                    "release": "v2.06"
                    "release": "v2.06AS"
                    "release": "v2.07"
                    "release": "v3.0"
                    "release": "v3.0B"
    
    Signed-off-by:  Paul A. Clarke <pc@us.ibm.com>

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ISA
 Power ISA
+
+# create ISA.json
+```
+$ (cat PPC1_ChFixedPt.mif PPC_ApInstMnem.mif ) | ./utils/mif2json.py > ./src/ISA.json
+```
--- utils/mif2json.py
+++ utils/mif2json.py
@@ -17,6 +17,22 @@ whitespace = re.compile('\s')
 wordchar = re.compile('\w')
 t = '    '
 
+class Mnemonic:
+	def __init__(self):
+		self.mnemonic = ''
+		self.rest = []
+		self.release = ''
+	def outputJSON(self,line_prefix,line_postfix):
+		print(line_prefix + t + t + "{" + line_postfix)
+		print(line_prefix + t + t + t + "\"mnemonic\": \"" + self.mnemonic + "\"",sep="",end="")
+		if len(self.rest) > 0:
+			print(",")
+			print(line_prefix + t + t + t + "\"regs\": \"" + str(self.rest) + "\"",sep="",end="")
+		print(",")
+		print(line_prefix + t + t + t + "\"release\": \"" + str(self.release) + "\"",sep="",end="")
+		print("")
+		print(line_prefix + t + t + "}",sep="",end="")
+
 class Instruction:
 	def __init__(self):
 		self.head = []
@@ -41,14 +57,7 @@ class Instruction:
 		comma = ''
 		for form in self.forms:
 			print(comma,sep="",end="")
-			print(line_prefix + t + t + "{" + line_postfix)
-			if len(form) > 0:
-				print(line_prefix + t + t + t + "\"mnemonic\": \"" + form[0] + "\"",sep="",end="")
-			if len(form) > 1:
-				print(",")
-				print(line_prefix + t + t + t + "\"regs\": \"" + str(form[1:]) + "\"",sep="",end="")
-			print("")
-			print(line_prefix + t + t + "}",sep="",end="")
+			form.outputJSON(line_prefix,line_postfix)
 			comma = "," + line_postfix + "\n"
 		print("")
 		print(line_prefix + t + "]" + line_postfix)
@@ -159,7 +168,10 @@ def ParaLine(f,tag):
 				c = f.read(1)
 				s = getString(f).strip()
 				if len(s) > 0:
-					inst.forms[len(inst.forms)-1].append(s.strip())
+					if len(inst.forms[len(inst.forms)-1].mnemonic) == 0:
+						inst.forms[len(inst.forms)-1].mnemonic = s.strip()
+					else:
+						inst.forms[len(inst.forms)-1].rest.append(s.strip())
 			elif tag == "code_example":
 				c = f.read(1)
 				# translate FrameMaker special characters with ASCII equivalents
@@ -188,7 +200,7 @@ def ParaLine(f,tag):
 			suppress = False
 		FindElementEnd(f)
 
-def PgfTag(f):
+def xTag(f):
 	global c,inst,insts
 	tag = ''
 	c = f.read(1)
@@ -208,13 +220,15 @@ def PgfTag(f):
 		insts.append(inst)
 	elif s in ["Instruction Form", ":p1.inst-syntax", ":p1.inst-syntax-compact"]:
 		tag = "Instruction Form"
-		inst.forms.append([])
+		inst.forms.append(Mnemonic())
 	elif s in [ "code_example", ":xmp." ]:
 		tag = "code_example"
 	elif s in [ "Body", ":p1." ]:
 		tag = "Body"
 	elif s in [ "Head_2_span" ]:
 		inst = ''
+	elif s in [ "instruction index" ]:
+		tag = s
 	return tag
 
 def Para(f):
@@ -230,7 +244,7 @@ def Para(f):
 				c = f.read(1)
 			token = getToken(f)
 		elif token == "PgfTag":
-			tag = PgfTag(f)
+			tag = xTag(f)
 		elif token == "Pgf":
 			while True:
 				try:
@@ -238,7 +252,7 @@ def Para(f):
 				except: break
 				token = getToken(f)
 				if token == "PgfTag":
-					tag = PgfTag(f)
+					tag = xTag(f)
 				FindElementEnd(f)
 		elif token == "ParaLine":
 			ParaLine(f,tag)
@@ -262,16 +276,142 @@ def TextFlow(f):
 			Para(f)
 		FindElementEnd(f)
 
+def cell_ParaLine(f):
+	global c
+	s = ''
+	while True:
+		try:
+			FindElementStart(f)
+		except: break
+		token = getToken(f)
+		if token == "String":
+			c = f.read(1)
+			s += getString(f)
+		FindElementEnd(f)
+	return s
+	
+def cell_Para(f):
+	global c
+	s = ''
+	while True:
+		try:
+			FindElementStart(f)
+		except: break
+		token = getToken(f)
+		if token == "ParaLine":
+			s += cell_ParaLine(f)
+		FindElementEnd(f)
+	return s
+	
+def CellContent(f):
+	global c
+	s = ''
+	while True:
+		try:
+			FindElementStart(f)
+		except: break
+		token = getToken(f)
+		if token == "Para":
+			s += cell_Para(f)
+		FindElementEnd(f)
+	return s
+	
+def Cell(f):
+	global c
+	s = ''
+	while True:
+		try:
+			FindElementStart(f)
+		except: break
+		token = getToken(f)
+		if token == "CellContent":
+			s += CellContent(f)
+		FindElementEnd(f)
+	return s
+
+def setISA(mnemonic,ISA):
+	for inst in insts:
+		for form in inst.forms:
+			if form.mnemonic == mnemonic:
+				form.release = ISA
+
+def Row(f):
+	global c
+	row = []
+	while True:
+		try:
+			FindElementStart(f)
+		except: break
+		token = getToken(f)
+		if token == "Cell":
+			row.append(Cell(f))
+		FindElementEnd(f)
+	mnemonics = [row[9]]
+	for n in re.finditer(r'\[(\w|\.)*\]',row[9]):
+		mnemonics_preexpand_list = []
+		for s in mnemonics:
+			mnemonics_preexpand_list.append(s)
+		mnemonics = []
+		for s in mnemonics_preexpand_list:
+			mnemonics.append(s.replace(n.group(0),'')) 
+			mnemonics.append(s.replace(n.group(0),n.group(1))) 
+	for mnemonic in mnemonics:
+		setISA(mnemonic,row[10])
+
+def TblBody(f):
+	global c
+	while True:
+		try:
+			FindElementStart(f)
+		except: break
+		token = getToken(f)
+		if token == "Row":
+			Row(f)
+		FindElementEnd(f)
+
+def Tbl(f):
+	global c
+	tag = ''
+	while True:
+		try:
+			FindElementStart(f)
+		except: break
+		token = getToken(f)
+		if token == "TblTag":
+			tag = xTag(f)
+		elif token == "TblBody":
+			if tag == "instruction index":
+				TblBody(f)
+		FindElementEnd(f)
+
+def Tbls(f):
+	global c
+	while True:
+		try:
+			FindElementStart(f)
+		except: break
+		token = getToken(f)
+		if token == "Tbl":
+			Tbl(f)
+		FindElementEnd(f)
+
 while c:
 	FindElementStart(f)
 
 	token = getToken(f)
 	if token == "TextFlow":
 		TextFlow(f)
+	elif token == "Tbls":
+		Tbls(f)
 	FindElementEnd(f)
 
 for inst in insts:
 	inst.head = [x.strip() for x in inst.head]
+	forms = []
+	for form in inst.forms:
+		if form.mnemonic != "":
+			forms.append(form)
+	inst.forms = forms
 
 print("[")
 comma=''
