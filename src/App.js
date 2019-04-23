a10b2379aa7c92e4e2cb3310f6141474661198e4
Paul A. Clarke <pc@us.ibm.com>
2019-04-23 15:26:03 -0500

    Add filter by ISA level
    Adding checkbox for each ISA level, so that instructions can be filtered.
    
    ISA levels are hardcoded instead of derived from ISA.json, because it is
    impossible to know the order otherwise.  (Maybe they could be pulled from
    a MIF file and put in their own JSON file?)
    
    An "all" checkbox is also added to facilitate quickly selecting or
    deselecting all other checkboxes.
    
    Signed-off-by:  Paul A. Clarke <pc@us.ibm.com>

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React, { Component } from "react";
 import "./App.scss";
 import Nav from "./Nav";
 import data from "./ISA.json";
-import { Accordion, AccordionItem, Search } from "carbon-components-react";
+import { Accordion, AccordionItem, Search, Checkbox } from "carbon-components-react";
 
 class App extends Component {
+
+    releases = [ "P1", "P2", "PPC", "v2.00", "v2.01", "v2.02", "v2.03", "v2.04", "v2.05", "v2.06", "v2.07", "v3.0", "v3.0B" ];
+
     constructor() {
         super();
         this.state = {
             data: data,
-            filter: ''
+            releaseSet: this.releases,
+            search: ''
         };
     }
 
@@ -82,31 +86,78 @@ class App extends Component {
         let allJson = [];
         for (let i = 0; i < data.length; i++) {
             for (let m = 0; m < data[i].mnemonics.length; m++) {
-                if (data[i].mnemonics[m].mnemonic.startsWith(this.state.filter)) {
-                    allJson.push(
-                        <AccordionItem
-                            title={data[i].description}
-                            onClick={e => {
-                                console.log("click");
-                            }}
-                            onHeadingClick={e => {
-                                console.log("heading click");
-                            }}
-                        >
-                            {this.genItem(data[i])}
-                        </AccordionItem>
-                    );
-                    break;
+                if (data[i].mnemonics[m].mnemonic.startsWith(this.state.search)) {
+                    if (this.state.releaseSet.includes(data[i].mnemonics[m].release)) {
+                        allJson.push(
+                            <AccordionItem
+                                title={data[i].description}
+                                onClick={e => {
+                                    console.log("click");
+                                }}
+                                onHeadingClick={e => {
+                                    console.log("heading click");
+                                }}
+                            >
+                                {this.genItem(data[i])}
+                            </AccordionItem>
+                        );
+                        break;
+                    }
                 }
             }
         }
         return allJson;
     };
 
-    filter() {
-        let id = document.getElementById("search-1");
-	this.setState({filter: id.value});
+    genReleaseCheckboxes() {
+        let all = [];
+        for (let i = 0; i < this.releases.length; i++) {
+            all.push(
+                <Checkbox defaultChecked
+                          className="checkbox"
+                          id={this.releases[i]}
+                          labelText={this.releases[i]}
+                          disabled={false}
+                          hideLabel={false}
+                          wrapperClassName=""
+                          onChange={e => {this.filter(e,this.releases[i])}}/>
+            );
+        }
+        return all;
     }
+
+    search() {
+        let id = document.getElementById("search-1");
+	this.setState({search: id.value});
+    }
+
+    filterAll(set) {
+        let newSet = [];
+	if (set) {
+            newSet = this.releases;
+        }
+        for (let i = 0; i < this.releases.length; i++) {
+            let id = document.getElementById(this.releases[i]);
+            id.checked = set;
+        }
+        this.setState({releaseSet: newSet});
+    }
+
+    filter(set,b) {
+        let newSet = [];
+	if (set) {
+            newSet = this.state.releaseSet;
+            newSet.push(b);
+        } else {
+            for (let i = 0; i < this.state.releaseSet.length; i++) {
+                if (this.state.releaseSet[i] === b)
+                    continue;
+                newSet.push(this.state.releaseSet[i]);
+            }
+        }
+        this.setState({releaseSet: newSet});
+    }
+
     render() {
         return (
             <div className="App">
@@ -120,6 +171,18 @@ class App extends Component {
                         <div className="mainContainer">
                             <div className="filterContainer">
                                 <h3>Filter stuff here</h3>
+                                <fieldset className="checkboxes">
+                                    <legend className="filter-heading">Restrict results to ISA levels:</legend>
+                                    <Checkbox defaultChecked
+                                              className="checkbox"
+                                              id="all-releases"
+                                              labelText="[all]"
+                                              disabled={false}
+                                              hideLabel={false}
+                                              wrapperClassName=""
+                                              onChange={e => {this.filterAll(e)}}/>
+                                    {this.genReleaseCheckboxes()}
+                                </fieldset>
                             </div>
                             <div className="accordianContainer">
                                 <div className="searchContainer">
