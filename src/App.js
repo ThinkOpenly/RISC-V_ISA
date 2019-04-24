bb7ded8df2cd857aaf459c42ac0e678561b3063a
Jake Stark <jake.stark@ibm.com>
2019-04-24 10:40:36 -0500

    changing pre to code snippet

--- src/App.js
+++ src/App.js
@@ -2,93 +2,138 @@ import React, { Component } from "react";
 import "./App.scss";
 import Nav from "./Nav";
 import data from "./ISA.json";
-import { Accordion, AccordionItem, Search, Checkbox } from "carbon-components-react";
+import {
+    Accordion,
+    AccordionItem,
+    Search,
+    Checkbox,
+    CodeSnippet
+} from "carbon-components-react";
 
 class App extends Component {
-
-    releases = [ "P1"    , "P2"    , "PPC"   , "v2.00" , "v2.01"  , "v2.02" , "v2.03" , "v2.04"  , "v2.05" , "v2.06" , "v2.07" , "v3.0"  , "v3.0B"  ];
-    cores =    [ "POWER1", "POWER2", "PPC970", "POWER4", "POWER4+", "POWER5", ""      , "POWER5+", "POWER6", "POWER7", "POWER8", "POWER9", "future" ];
+    releases = [
+        "P1",
+        "P2",
+        "PPC",
+        "v2.00",
+        "v2.01",
+        "v2.02",
+        "v2.03",
+        "v2.04",
+        "v2.05",
+        "v2.06",
+        "v2.07",
+        "v3.0",
+        "v3.0B"
+    ];
+    cores = [
+        "POWER1",
+        "POWER2",
+        "PPC970",
+        "POWER4",
+        "POWER4+",
+        "POWER5",
+        "",
+        "POWER5+",
+        "POWER6",
+        "POWER7",
+        "POWER8",
+        "POWER9",
+        "future"
+    ];
 
     constructor() {
         super();
         this.state = {
             data: data,
             releaseSet: this.releases,
-            search: ''
+            search: ""
         };
     }
 
     displayRegs(regs) {
-        let all = []
+        let all = "";
         for (let i = 0; i < regs.length; i++) {
-            all.push(
-                <td className="mnemonic">{regs[i]}</td>
-            );
+            all += regs[i] + ", ";
         }
-        return(all);
+        return all.substring(0, all.length - 2);
     }
 
     displayMnemonics(item) {
         let all = [];
+
         for (let i = 0; i < item.mnemonics.length; i++) {
             all.push(
-                <tr>
-                <td className="mnemonic">{item.mnemonics[i].mnemonic}</td>
-                {this.displayRegs(item.mnemonics[i].regs)}
-                </tr>
+                item.mnemonics[i].mnemonic +
+                    "       " +
+                    this.displayRegs(item.mnemonics[i].regs) +
+                    "\n\r"
             );
         }
-        return (all);
+        return all;
     }
 
     displayCode(item) {
         let all = [];
         for (let i = 0; i < item.code.length; i++) {
-            all.push(
-                <pre className="code">
-                {item.code[i]}
-                </pre>
-            );
+            all.push(item.code[i] + "\n");
         }
-        return (all);
+        console.log(all);
+
+        return all;
     }
 
     displayBody(item) {
         let all = [];
         for (let i = 0; i < item.body.length; i++) {
-            all.push(
-                <p>{item.body[i]}</p>
-            );
+            all.push(<p>{item.body[i]}</p>);
         }
-        return (all);
+        return all;
     }
 
     genItem(item) {
         return (
             <div className="expandContainer">
-                    <div className="expandContainer">
-                        <div className="column">
-                            <h4>Mnemonics:</h4>
-                            <table>
-                                <tbody>
-                                {this.displayMnemonics(item)}
-                                </tbody>
-                            </table>
-                            <h4>Code:</h4>
-                            {this.displayCode(item)}
-                            {this.displayBody(item)}
-                        </div>
-                    </div>
+                <div className="column">
+                    <h4>Mnemonics:</h4>
+                    <CodeSnippet
+                        feedback="Copied to clipboard"
+                        copyButtonDescription="Copy"
+                        ariaLabel="mneumonic"
+                        type="inline"
+                        onClick={() => {
+                            console.log("clicked");
+                        }}
+                        type={item.mnemonics.length > 1 ? "multi" : "inline"}
+                    >
+                        {this.displayMnemonics(item)}
+                    </CodeSnippet>
+                    <br />
+                    <br />
+                    <h4>Code:</h4>
+                    <CodeSnippet
+                        type="multi"
+                        feedback="Copied to clipboard"
+                        onClick={() => {
+                            console.log("clicked");
+                        }}
+                    >
+                        {this.displayCode(item)}
+                    </CodeSnippet>
+                    <br />
+                    <br />
+                    {this.displayBody(item)}
+                </div>
             </div>
         );
     }
 
     genTitle(item) {
         let s = "";
-        let comma = '';
+        let comma = "";
         for (let i = 0; i < item.mnemonics.length; i++) {
             s = s + comma + item.mnemonics[i].mnemonic;
-            comma = ', ';
+            comma = ", ";
         }
         return (
             <table className="item">
@@ -98,15 +143,22 @@ class App extends Component {
                         <td className="itemtitlemnem">{s}</td>
                     </tr>
                 </tbody>
-            </table>);
+            </table>
+        );
     }
 
     genData = data => {
         let allJson = [];
         for (let i = 0; i < data.length; i++) {
             for (let m = 0; m < data[i].mnemonics.length; m++) {
-                if (data[i].mnemonics[m].mnemonic.startsWith(this.state.search)) {
-                    if (this.state.releaseSet.includes(data[i].mnemonics[m].release)) {
+                if (
+                    data[i].mnemonics[m].mnemonic.startsWith(this.state.search)
+                ) {
+                    if (
+                        this.state.releaseSet.includes(
+                            data[i].mnemonics[m].release
+                        )
+                    ) {
                         allJson.push(
                             <AccordionItem
                                 title={this.genTitle(data[i])}
@@ -140,19 +192,23 @@ class App extends Component {
             </table>
         );
     }
-            
+
     genReleaseCheckboxes() {
         let all = [];
         for (let i = 0; i < this.releases.length; i++) {
             all.push(
-                <Checkbox defaultChecked
-                          className="checkbox"
-                          id={this.releases[i]}
-                          labelText={this.genReleaseLabel(i)}
-                          disabled={false}
-                          hideLabel={false}
-                          wrapperClassName=""
-                          onChange={e => {this.filter(e,this.releases[i])}}/>
+                <Checkbox
+                    defaultChecked
+                    className="checkbox"
+                    id={this.releases[i]}
+                    labelText={this.genReleaseLabel(i)}
+                    disabled={false}
+                    hideLabel={false}
+                    wrapperClassName=""
+                    onChange={e => {
+                        this.filter(e, this.releases[i]);
+                    }}
+                />
             );
         }
         return all;
@@ -160,34 +216,33 @@ class App extends Component {
 
     search() {
         let id = document.getElementById("search-1");
-	this.setState({search: id.value});
+        this.setState({ search: id.value });
     }
 
     filterAll(set) {
         let newSet = [];
-	if (set) {
+        if (set) {
             newSet = this.releases;
         }
         for (let i = 0; i < this.releases.length; i++) {
             let id = document.getElementById(this.releases[i]);
             id.checked = set;
         }
-        this.setState({releaseSet: newSet});
+        this.setState({ releaseSet: newSet });
     }
 
-    filter(set,b) {
+    filter(set, b) {
         let newSet = [];
-	if (set) {
+        if (set) {
             newSet = this.state.releaseSet;
             newSet.push(b);
         } else {
             for (let i = 0; i < this.state.releaseSet.length; i++) {
-                if (this.state.releaseSet[i] === b)
-                    continue;
+                if (this.state.releaseSet[i] === b) continue;
                 newSet.push(this.state.releaseSet[i]);
             }
         }
-        this.setState({releaseSet: newSet});
+        this.setState({ releaseSet: newSet });
     }
 
     render() {
@@ -202,17 +257,22 @@ class App extends Component {
                         <Nav />
                         <div className="mainContainer">
                             <div className="filterContainer">
-                                <h3>Filter stuff here</h3>
                                 <fieldset className="checkboxes">
-                                    <legend className="filter-heading">Restrict results to ISA levels:</legend>
-                                    <Checkbox defaultChecked
-                                              className="checkbox"
-                                              id="all-releases"
-                                              labelText="[all]"
-                                              disabled={false}
-                                              hideLabel={false}
-                                              wrapperClassName=""
-                                              onChange={e => {this.filterAll(e)}}/>
+                                    <legend className="filter-heading">
+                                        Restrict results to ISA levels:
+                                    </legend>
+                                    <Checkbox
+                                        defaultChecked
+                                        className="checkbox"
+                                        id="all-releases"
+                                        labelText="[all]"
+                                        disabled={false}
+                                        hideLabel={false}
+                                        wrapperClassName=""
+                                        onChange={e => {
+                                            this.filterAll(e);
+                                        }}
+                                    />
                                     {this.genReleaseCheckboxes()}
                                 </fieldset>
                             </div>
@@ -226,7 +286,7 @@ class App extends Component {
                                         closeButtonLabelText=""
                                         placeHolderText="Search"
                                         onChange={() => {
-                                            this.search()
+                                            this.search();
                                         }}
                                         id="search-1"
                                     />
