48593820a63453e7d8d7dc1e16b19bfbff2b2177
Paul A. Clarke <pc@us.ibm.com>
2021-01-15 15:30:03 -0600

    Yet another tag for Instruction Form
    ...which pulls in a few more missing instructions.
    
    Signed-off-by: Paul A. Clarke <pc@us.ibm.com>

--- utils/mif2json.py
+++ utils/mif2json.py
@@ -359,7 +359,7 @@ def xTag(f):
 		except: pass
 		inst = Instruction();
 		insts.append(inst)
-	elif s in [ "Instruction Form", ":p1.inst-syntax", ":p1.inst-syntax-compact", ":p1.inst-syntax.wide" ]:
+	elif s in [ "Instruction Form", ":p1.inst-syntax", ":p1.inst-syntax-compact", ":p1.inst-syntax.wide", ":p1.inst-syntax compact" ]:
 		tag = "Instruction Form"
 		inst.mnemonics.append(Mnemonic())
 
