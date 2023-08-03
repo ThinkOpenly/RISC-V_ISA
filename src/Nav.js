a1ab73261235c801172569aca4996b10edfefa5f
Paul A. Clarke <pclarke@ventanamicro.com>
2023-08-03 14:19:43 -0500

    Update content for RISC-V
    Signed-off-by: Paul A. Clarke <pclarke@ventanamicro.com>

--- src/Nav.js
+++ src/Nav.js
@@ -16,16 +16,16 @@ class Nav extends Component {
     render() {
         return (
             <div>
-                <Header aria-label="PowerISA">
-                    <HeaderName href="#" prefix={"Power"}>
+                <Header aria-label="RISC-V ISA">
+                    <HeaderName href="#" prefix={"RISC-V"}>
                         ISA Reference
                     </HeaderName>
                     <HeaderNavigation aria-label="ISA">
                         <HeaderMenuItem>|</HeaderMenuItem>
-                        <HeaderMenuItem href="https://www.IBM.com">IBM</HeaderMenuItem>
-                        <HeaderMenuItem href="https://OpenPOWERFoundation.org">The OpenPOWER Foundation</HeaderMenuItem>
-                        <HeaderMenuItem href="https://www.ibm.com/systems/power/openpower/">IBM Portal for OpenPOWER</HeaderMenuItem>
-                        <HeaderMenuItem href="https://developer.ibm.com/linuxonpower/">Linux on IBM Power Developer Portal</HeaderMenuItem>
+                        <HeaderMenuItem href="https://riscv.org">RISC-V International</HeaderMenuItem>
+                        <HeaderMenuItem href="https://riscv.org/technical/specifications/">RISC-V Specifications</HeaderMenuItem>
+                        <HeaderMenuItem href="https://GitHub.com/riscv/sail-riscv">RISC-V Sail Model</HeaderMenuItem>
+                        <HeaderMenuItem href="https://GitHub.com/ThinkOpenly/riscvdecode">RISC-V Sail to JSON</HeaderMenuItem>
                     </HeaderNavigation>
                 </Header>
             </div>
